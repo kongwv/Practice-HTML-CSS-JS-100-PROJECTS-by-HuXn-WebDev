20ce9bf210cb2e1b052cb86f8c98dd07f6d03d2a
kongwv <116346840+kongwv@users.noreply.github.com>
2025-03-06 02:41:58 +0700

    Fade Out Project

---
+++ 022.fade-out/app.js
@@ -0,0 +1,14 @@
+let arrow = document.getElementById("arrow");
+
+window.addEventListener("scroll", () => {
+  let position = window.scrollY;
+  console.log(position);
+
+  if (position <= 50) {
+    arrow.classList.add("fade-in");
+    arrow.classList.remove("fade-out");
+  } else {
+    arrow.classList.add("fade-out");
+    arrow.classList.remove("fade-in");
+  }
+});
